03ca0824c39e4443c1797c4400b3c001822e4f09
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-11-19 20:38:37 +0200

    move gitignore

--- backend/id_checker/id_search/views.py
+++ backend/id_checker/id_search/views.py
@@ -7,6 +7,7 @@ from rest_framework.decorators import api_view
 from .models import IDRecord, PublicHoliday
 from .serializers import IDRecordSerializer, PublicHolidaySerializer
 
+
 @api_view(['POST'])
 def id_search(request):
     id_number = request.data.get('idNumber')
@@ -14,3 +15,7 @@ def id_search(request):
     if len(id_number) != 13:
         return Response({'error': 'Invalid Id number'}, status=status.HTTP_400_BAD_REQUEST)
     
+    # TODO: 
+    # 1. implement decoding logic for id number.
+    # 2. fetch public holidays using the Calendarific API.
+    # 3. Return the results to the frontend.
