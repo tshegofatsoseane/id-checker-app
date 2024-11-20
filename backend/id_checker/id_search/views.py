cc9ab15e0d5b6e4c291bf539f4a8fcd7fce2ea16
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-11-20 23:22:26 +0200

    created luhn_validator to decode id components

--- backend/id_checker/id_search/views.py
+++ backend/id_checker/id_search/views.py
@@ -1,3 +1,4 @@
+import datetime
 from django.shortcuts import render
 
 import requests
@@ -7,6 +8,55 @@ from rest_framework.decorators import api_view
 from .models import IDRecord, PublicHoliday
 from .serializers import IDRecordSerializer, PublicHolidaySerializer
 
+
+# Luhn algorithm
+def luhn_validator(id_number):
+    """validate id number using luhn algorithm"""
+    total = 0
+    reverse_digits = id_number[::-1]
+    for i, digit in enumerate(reverse_digits):
+        n = int(digit)
+        if i % 2 == 1: 
+            n *= 2
+            if n > 9:
+                n -= 9
+        total += n
+    return total % 10 == 0
+
+def decode_id_number(id_number):
+    """decode iD number and return it's parts/components"""
+    if len(id_number) != 13 or not id_number.isdigit():
+        raise ValueError("Invalid ID number format")
+
+    # extract parts/components from the id no.
+    birth_str = id_number[:6] 
+    gender_code = int(id_number[6:10])  
+    citizenship_code = int(id_number[10]) 
+    checksum = int(id_number[12])
+
+    if not luhn_validator(id_number):
+        raise ValueError("Invalid ID number checksum")
+
+    birth_date = datetime.strptime(birth_str, '%y%m%d').date()
+
+    if gender_code >= 5000:
+        gender = "Male"
+    else:
+        gender = "Female"
+
+     
+    if citizenship_code == 0:
+        sa_citizen = True
+    else:
+        sa_citizen = False
+
+    return {
+        'date_of_birth': birth_date,
+        'gender': gender,
+        'sa_citizen': sa_citizen
+    }
+
+
 @api_view(['POST'])
 def id_search(request):
     id_number = request.data.get('idNumber')
@@ -14,7 +64,4 @@ def id_search(request):
     if len(id_number) != 13:
         return Response({'error': 'Invalid Id number'}, status=status.HTTP_400_BAD_REQUEST)
     
-    # TODO: 
-    # 1. implement decoding logic for id number.
-    # 2. fetch public holidays using the Calendarific API.
-    # 3. Return the results to the frontend.
+
