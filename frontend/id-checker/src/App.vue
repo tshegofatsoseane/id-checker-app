f6726c45824d05d740f8a7d67ea1c3dd75d729ab
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-11-21 20:14:23 +0200

    feat: initialize frontend

--- frontend/id-checker/src/App.vue
+++ frontend/id-checker/src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="12" sm="8">
+          <SearchID />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchID from "@/components/SearchID.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
-</script>
+    SearchID,
+  },
+};
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+</script>
